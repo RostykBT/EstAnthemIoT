4343ae93d20cba5361b1e0aba1f587aa9c5f5868
ivsymo <ivsymo@ttu.ee>
2023-04-03 18:06:11 +0300

    Release

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 
 #include <Arduino.h>
 #include <Buzzer.h>
+#include <ittiot.h>
+
 #include "notes.h"
 
 #define REST 0
@@ -12,7 +14,14 @@
 #define HALF_TIME 1666       // Time of a half note in microseconds
 
 
-//estonian anthem melody
+#define MOTION_TOPIC "ESP23/motion"
+#define WIFI_NAME "TalTech"
+#define WIFI_PASSWORD ""
+
+//Pin definition for the buzzer (GPIO15)
+#define BUZZER_PIN D8
+
+//Estonian anthem melody
 int melody[] = { 
   NOTE_E4, EIGHTH_TIME,
   NOTE_C4, EIGHTH_TIME,
@@ -25,25 +34,67 @@ int melody[] = {
   NOTE_A4, QUARTER_TIME,
 };
 
- 
-
-//Pin definition for the buzzer (GPIO15)
-#define BUZZER_PIN D8
+bool isPlaying = false;
  
 Buzzer buzzer(BUZZER_PIN);
 
-void setup()
+void iot_received(String topic, String msg)
+{ 
+  // Custom
+  if(topic == MOTION_TOPIC)
+  {
+    if(msg == "1")
+    {
+      isPlaying = true;
+    }
+  }
+  
+}
+ 
+void iot_connected()
 {
-  Serial.begin(115200); // setting up serial connection parameter
-  Serial.println("Booting");
+  Serial.println("MQTT connected callback");
 
+  // Custom
+  iot.subscribe(MOTION_TOPIC);
+}
+
+// Custom function
+void playAnthem()
+{
   for(int i = 0; i < 18 ; i+=2)
   {
-    buzzer.sound(melody[i], melody[i+1]); // Play the melody
+    buzzer.sound(melody[i], melody[i+1]); 
   } 
+}
+
+void setup()
+{
+  Serial.begin(115200); 
+  Serial.println("Booting");
+ 
+  iot.setConfig("wname", WIFI_NAME);
+  iot.setConfig("wpass", WIFI_PASSWORD);
+  iot.setConfig("msrv", "193.40.245.72");
+  iot.setConfig("mport", "1883");
+  iot.setConfig("muser", "test");
+  iot.setConfig("mpass", "test");
+
+
+  iot.printConfig();
+  iot.setup(); 
 }
 
 void loop()
 {
-  delay(200); // Wait for 0.2 second
+  iot.handle(); 
+  delay(200);
+
+
+  // Custom
+  if(isPlaying)
+  {
+    playAnthem();
+    isPlaying = false;
+  }
 }
